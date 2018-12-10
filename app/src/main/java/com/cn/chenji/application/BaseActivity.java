f7cd2db11e2f421877803a59fef604a08169d230
chenji@f-road.com.cn <chenji@f-road.com.cn>
2018-12-10 18:28:47 +0800

    first commit my project

---
+++ app/src/main/java/com/cn/chenji/application/BaseActivity.java
@@ -0,0 +1,30 @@
+package com.cn.chenji.application;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import java.io.File;
+
+/**
+ * author by chenji on 2018/12/6
+ */
+public class BaseActivity extends Activity {
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext=getApplicationContext();
+    }
+
+    public void initImageLoader(Context context) {
+        //   This configuration tuning is custom. You can tune every option, you may tune some of them,
+        //   or you can create default configuration by
+        //  ImageLoaderConfiguration.createDefault(this);
+        //  method.
+
+    }
+
+}
---
+++ app/src/main/java/com/cn/chenji/application/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.cn.chenji.application;
+
+import android.app.Application;
+
+/**
+ * author by chenji on 2018/12/6
+ */
+public class BaseApplication  extends Application{
+
+    //在这里完成一些基本设置，例如内存等等
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
